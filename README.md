562f83bd23fc00fa2e25ba7688f6ca9795956228
Albin Heimerson <albin.heimerson@control.lth.se>
2023-02-22 22:24:42 +0100

    halfway through testing

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 This repo contains my code following the tutorial for writing a simple OS in Rust found [here](https://os.phil-opp.com/).
 
 ## Setup
-Need rustup, cargo, some specific toolchains from rustup and other stuff?
+Install `qemu-system-x86_64` and `rustup`.
+
+From `rustup` you should have `cargo`, so you can run `cargo install bootimage`.
 
 ## Running
 
-`cargo run` automatically builds and opens qemu
+`cargo run` automatically builds and opens qemu.
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,73 @@
+// All #! expressions should be at start of file?
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+#![feature(custom_test_frameworks)] // Include feature custom test framework
+#![test_runner(crate::test_runner)] // Register our test runner for custom test framework
+#![reexport_test_harness_main = "test_main"] // Redefines test entry point
+
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
+mod serial;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    #[cfg(test)]
+    test_main(); // Call new test entrypoint, but only if testing
+    
     loop {}
 }
 
-/// This function is called on panic.
+/// Normal panic handler
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+/// Test panic handler
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+// Test runner
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
 }
