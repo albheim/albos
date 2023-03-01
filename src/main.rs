bf98007e20a05cefffca55bc717789b210e7a66d
Albin Heimerson <albin.heimerson@control.lth.se>
2023-03-01 22:23:52 +0100

    testing chapter done

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@
 #![no_main] // disable all Rust-level entry points
 
 #![feature(custom_test_frameworks)] // Include feature custom test framework
-#![test_runner(crate::test_runner)] // Register our test runner for custom test framework
+#![test_runner(albos::test_runner)] // Register our test runner for custom test framework
 #![reexport_test_harness_main = "test_main"] // Redefines test entry point
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-mod serial;
+use albos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -29,45 +27,8 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// Test panic handler
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-// Test runner
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    serial_println!("[ok]");
+    albos::test_panic_handler(info)
 }
